f3718b6feb573d99d3f85d28e5b4e51cf288c35a
Md. Jobayer Islam <jobayerccj@gmail.com>
2015-03-05 18:56:48 +0600

    slider2 updated.

--- js/jquery.flexisel.js
+++ js/jquery.flexisel.js
@@ -104,7 +104,7 @@
                     $(".nbs-flexisel-item img").css("max-width", baseWidth);
                     $(".nbs-flexisel-item img").css("max-height", baseHeight);
                 }
-                $("<div class='nbs-flexisel-nav-left'><i class='fa fa-arrow-circle-left'></i></div><div class='nbs-flexisel-nav-right'><i class='fa fa-arrow-circle-right'></i></div>").insertAfter(flexiselInner);
+                $("<div class='nbs-flexisel-nav-left' ><i class='fa fa-arrow-circle-left'></i></div><div class='nbs-flexisel-nav-right'><i class='fa fa-arrow-circle-right'></i></div>").insertAfter(flexiselInner);
                 if (settings.clone) {
                     var cloneContent = object.children().clone();
                     object.append(cloneContent);
