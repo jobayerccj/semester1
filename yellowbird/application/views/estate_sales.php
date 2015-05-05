8ffc1ddec9e00e7f09372f10c378ab40df2b0a3a
Md. Jobayer Islam <jobayerccj@gmail.com>
2015-05-05 16:40:10 +0600

    estate sales text updated.

--- yellowbird/application/views/estate_sales.php
+++ yellowbird/application/views/estate_sales.php
@@ -17,19 +17,19 @@
                 </div>
                 
                <div class="col-lg-9">
-                   <h1>Lorem ipsum dolor sit amet, consectetuer adipiscing</h1>
-                   <p>aliquam erat volutpat. Ut wisi enim ad minim veniam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation</p>
+                   <h1>Estate Sales and Liquidation Services</h1>
+                   <p>We Provide Many Resources to Those Looking to Sel Their Estate Including:</p>
                    <ul>
-                       <li>nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. </li>
-                       <li>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat</li>
-                       <li>nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</li>
-                       <li>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat</li>
-                       <li>nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</li>
-                       <li>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat</li>
-                       <li>nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</li>
-                       <li>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat</li>
-                       <li>nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</li>
-                       <li>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat</li>
+                       <li>Free Consultation to help you determine the best course of action for your Estate needs. </li>
+                       <li>We can help you with the entire Estate: Personal Property, Automibiles, Campers, Jewelry, Firearms, Real Estate ... and mcuh more.</li>
+                       <li>Pre-Sale Clean-outs & Trash Removal as needed.</li>
+                       <li>Sort, Display and Price all items to enhance the value of each item.</li>
+                       <li>Massive Exposure in Estatesales.net, EstateSales.org, Local Newspapers, Craigslist, and More...PLUS we send notification to our own LARGE Customer E-mail list.</li>
+                       <li>Yard and Directional Signs based on local codes and laws.</li>
+                       <li>Depending on your wishes we can offer complete clean-outs upon sales completion.</li>
+                       <li>Donations to local charties for tax credits back to you of any items that do not sell (Goodwill, Salvation Army, Veterans Associations, ReStore and others).</li>
+                       <li>We also offer you a wide range of Real Estate Services (We do it all, everything from buying your house in "As Is" condition, to helping you bring your home up to date and ready for sale, to listing your Home for Sale.</li>
+                       <li>We follow Uniform Standards of Professional Appraisal Practice (USPAP) guidelines.</li>
                    </ul>
                 </div>
             </div>
