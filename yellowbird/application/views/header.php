133f90a9c45d3f4de650e18fd2d2a1234ce3480f
Md. Jobayer Islam <jobayerccj@gmail.com>
2015-05-10 15:33:03 +0600

    new logo & title added.

--- yellowbird/application/views/header.php
+++ yellowbird/application/views/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>title</title>
+        <title>Yellow Bird Estate Sales Since 2012</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         
@@ -61,26 +61,32 @@
 
             <div class="container">
                 <div class="row ">
-                    <div class="col-lg-2 col-md-3 col-sm-3">
+                    <div class="col-lg-3 col-md-3 col-sm-3">
                         <a href="#">
                             <img src="<?php echo base_url();?>images/virtual-properties-realty.gif" alt="virtual-properties-realty" style="width: 100%;"/>
                         </a>
                     </div>
-                    <div class="col-lg-1 col-md-2 col-sm-2">
+                    <div class="col-lg-2 col-md-2 col-sm-2">
                         <a href="http://www.gainesvillehomeexpo.com/">
                             <img src="<?php echo base_url();?>images/hba.jpg" alt="Home Builders" style="width: 100%;"/>
                         </a>
                     </div>
-                    <div class="col-lg-1 col-md-1 col-sm-2" style="padding-left: 0;">
+                    <div class="col-lg-1 col-md-1 col-sm-2" style="">
                         <a href="http://www.aselonline.com/">
                             <img src="<?php echo base_url();?>images/american-society-of-estate.jpg" alt="american-society-of-estate"/>
                         </a>
                     </div>
-                    <div class="col-lg-3 col-md-3 col-sm-3">
+                    <div class="col-lg-2 col-md-2 col-sm-2 banner_img3">
                         <br/>
                         <a id="bbblink" class="ruhzbum" href="http://www.bbb.org/atlanta/business-reviews/estate-liquidators/yellow-bird-antiques-and-interiors-in-dawsonville-ga-27495525#bbbseal" title="Yellow Bird Antiques & Interiors, LLC is a BBB Accredited Estate Liquidator in Dawsonville, GA" style="display: block;position: relative;overflow: hidden; width: 150px; height: 68px; margin: 0px; padding: 0px;"><img style="padding: 0px; border: none;" id="bbblinkimg" src="http://seal-atlanta.bbb.org/logo/ruhzbum/yellow-bird-antiques-and-interiors-27495525.png" width="300" height="68" alt="Yellow Bird Antiques & Interiors, LLC is a BBB Accredited Estate Liquidator in Dawsonville, GA" /></a><script type="text/javascript">var bbbprotocol = ( ("https:" == document.location.protocol) ? "https://" : "http://" ); document.write(unescape("%3Cscript src='" + bbbprotocol + 'seal-atlanta.bbb.org' + unescape('%2Flogo%2Fyellow-bird-antiques-and-interiors-27495525.js') + "' type='text/javascript'%3E%3C/script%3E"));</script>
                     </div>
-                
+                    
+                    <div class="col-lg-3 col-md-3 col-sm-3 banner_img4">
+                        <br/>
+                        <a href="http://www.aoaonline.org/">
+                            <img src="<?php echo base_url();?>images/Association of Online Appraisers.jpg" alt="Association of Online Appraisers" style="width: 100%;"/>
+                        </a>
+                    </div>
                 </div>
             </div>
         </header>
